8498626812756b1bbf189b133a41ad5c91b40be6
yusa0730 <isizawa5226@gmail.com>
2020-03-10 22:01:09 +0900

    first-commit

---
+++ vue_hello/webpack.mix.js
@@ -0,0 +1,13 @@
+let mix = require('laravel-mix');
+
+mix.js('resources/js/home.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css', {
+      implementation: require('node-sass')
+    });
+
+mix.browserSync({
+  files: [
+    "public/**/*.*"
+  ],
+  proxy: "http://localhost:8000"
+});
